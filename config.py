4ba5e3ebc0ec41b7bc9137a27949597437856917
M0NST3R-666 <redmonster3301@gmail.com>
2025-05-28 23:53:31 +0530

    update

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ BOT_ID = getenv("BOT_ID")
 # -------------------------------------------------------
 OWNER_USERNAME = getenv("OWNER_USERNAME","ZeoXpro")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME" , "AqiaBot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "Streamvc_bot")
 # --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME" , "Aqia")
 # ---------------------------------------------------------
